6c37813dc20d64cd395d58e0ab973b2a9e7ecd00
dablaze <chukwuwauchenna@gmail.com>
2020-09-27 00:07:30 +0100

    added textFormFields to record user input and a fab to save the input

--- lib/page/add_bookmark_page.dart
+++ lib/page/add_bookmark_page.dart
@@ -7,6 +7,36 @@ class AddBookMarkPage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Add a new Bookmark"),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.check),
+        backgroundColor: Colors.green,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextFormField(
+              autofocus: true,
+              decoration: InputDecoration(
+                  icon: Icon(Icons.title),
+                  labelText: "Title",
+                  hintText: "Title of the bookmark",
+                  border: OutlineInputBorder()),
+            ),
+            SizedBox(
+              height: 16.0,
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                  icon: Icon(Icons.link),
+                  labelText: "URL",
+                  hintText: "WebPage Link",
+                  border: OutlineInputBorder()),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
