f52ceaffb224f876050aa5d843ea257d964b4873
dablaze <chukwuwauchenna@gmail.com>
2020-09-26 23:50:47 +0100

    added a progress bar on the page that opens each bookmark

--- lib/page/view_bookmark_page.dart
+++ lib/page/view_bookmark_page.dart
@@ -1,8 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-class ViewBookmarkedPage extends StatelessWidget {
+class ViewBookmarkedPage extends StatefulWidget {
+  @override
+  _ViewBookmarkedPageState createState() => _ViewBookmarkedPageState();
+}
+
+class _ViewBookmarkedPageState extends State<ViewBookmarkedPage> {
+  var _isLoadingWebPage = true;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("ViewBookmark")));
+    return Scaffold(
+      appBar: AppBar(title: Text("View Bookmark")),
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          WebView(
+              onPageFinished: (url) {
+                setState(() {
+                  _isLoadingWebPage = false;
+                });
+              },
+              initialUrl: "https://google.com"),
+          _isLoadingWebPage ? CircularProgressIndicator() : Container(),
+        ],
+      ),
+    );
   }
 }
