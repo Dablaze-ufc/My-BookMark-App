4aad9e84a5621bb8f28131408618df79dedbbedc
dablaze <chukwuwauchenna@gmail.com>
2020-09-28 00:06:07 +0100

    implemented the bookmark page using the ListView.builder method to ensure effective lists

--- lib/widgets/bookmark_list_widget.dart
+++ lib/widgets/bookmark_list_widget.dart
@@ -4,22 +4,17 @@ import 'package:new_project/model/bookmark.dart';
 import 'bookmark_list_item_widget.dart';
 
 class BookmarksListWidget extends StatelessWidget {
-  List<Bookmark> bookmarksList;
+  final List<Bookmark> bookmarksList;
 
   BookmarksListWidget(this.bookmarksList);
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: getBookmarkWidgetList(),
+    return ListView.builder(
+      itemCount: bookmarksList.length,
+      itemBuilder: (BuildContext context, int index) {
+        return BookmarkListItemWidget(bookmarksList[index]);
+      },
     );
-  }
-
-  List<BookmarkListItemWidget> getBookmarkWidgetList() {
-    List<BookmarkListItemWidget> list = [];
-    for (Bookmark bookmark in bookmarksList) {
-      list.add(BookmarkListItemWidget(bookmark));
-    }
-    return list;
   }
 }
